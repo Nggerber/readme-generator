6c23f91c9aa8dde669f1f73c1db235569fc51a80
nggerber <gerberclimbs@gmail.com>
2020-09-25 15:49:48 -0600

    added generate readme functionality

--- index.js
+++ index.js
@@ -5,72 +5,50 @@ var axios = require("axios");
 var fs = require("fs");
 
 // Require all npm packages and files
+function promptUser() {
 
-inquirer.prompt([
-  // questions to user using "inquirer"
-  {
-    type: "input",
-    message: "What is the name of your current project?",
-    name: 'projectTitle',
-    default: "Readme Generator"
-  },
+  inquirer.prompt([
+    // questions to user using "inquirer"
+    {
+      type: "input",
+      message: "What is the name of your current project?",
+      name: 'projectTitle',
+      default: "Readme Generator"
+    },
 
-  {
-    type: "input",
-    message: "Give a quick description of your project?",
-    name: "description",
-    default: "When you enter the title description and repository name this application creates a README.md"
-  },
+    {
+      type: "input",
+      message: "Give a quick description of your project?",
+      name: "description",
+      default: "When you enter the title description and repository name this application creates a README.md"
+    },
 
-  {
-    type: "checkbox",
-    message: "Choose a license for your project",
-    name: "license",
-    choices: [
-      "jjhuj",
-      "dsfhdfhdjf",
-      "djfbdjhfjdh",
-      'djfhdjhfdjhf',
-    ],
-    default: "GoodREADMEGenerator"
-  },
+    {
+      type: "checkbox",
+      message: "Choose a license for your project",
+      name: "license",
+      choices: [
+        "jjhuj",
+        "dsfhdfhdjf",
+        "djfbdjhfjdh",
+        'djfhdjhfdjhf',
+      ],
+      default: "GoodREADMEGenerator"
+    },
 
-  {
-    type: "input",
-    message: "What is your GitHub username?",
-    name: "username",
-    default: "Nggerber"
-  },
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "userName",
+      default: "Nggerber"
+    },
 
-  {
-    type: "input",
-    message: "What is your email address?",
-    name: "install",
-    default: "neal.g.gerber@gmail.com"
-  },
-
-  {
-    type: "input",
-    message: "Write instructions for using your project.",
-    name: "usage",
-    default:
-      "1.Run node index.js 2.Answers the questions 3.The README.md file will be created. "
-  },
-
-  {
-    type: "input",
-    message:
-      "please enter git hub user names of the contributor if any (If there are mulitple contributor, seperate names with comma and no space! )",
-    name: "contributors",
-    default: " Robert McKenney, Abdul Amoud and Igor Calvacante"
-  },
-
-  {
-    type: "input",
-    message: "Provide examples on how to run tests.",
-    name: "test",
-    default: "Insert your tests sample here..."
-  }
-]);
-
+    {
+      type: "input",
+      message: "What is your email address?",
+      name: "email",
+      default: "neal.g.gerber@gmail.com"
+    },
+  ]);
+}
 
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,34 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-    return `# ${data.title}
+
+    return `
+
+    #${data.projectTitle};
+
+// ![](https://img.shields.io/badge/license-${(data.license).replace(/\s/g, "%20")}-yellow)\n
+
+${data.description}
+
+## Table of Contents 
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+
+
+## License
+
+This project is licensed by [${data.license}](${data.printLicense})
+
+
+## Questions
+
+[Github profile](https://github.com/${data.userName})
+
+[E-Mail](mailto:${data.email})
   
   `;
   }
